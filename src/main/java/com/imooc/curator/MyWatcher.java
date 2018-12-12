10780f9626510593de7c55c20cf3cb6c48b016e0
ylz <2357818962@qq.com>
2018-12-12 14:12:22 +0800

    gg

---
+++ src/main/java/com/imooc/curator/MyWatcher.java
@@ -0,0 +1,14 @@
+package com.imooc.curator;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class MyWatcher implements Watcher {
+
+
+	public void process(WatchedEvent event) {
+		System.out.println("触发watcher，节点路径为：" + event.getPath());
+	}
+
+
+}
--- src/main/java/com/imooc/utils/AclUtils.java
+++ src/main/java/com/imooc/utils/AclUtils.java
@@ -1,14 +1,21 @@
 package com.imooc.utils;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 
-import java.io.IOException;
-
 public class AclUtils {
 	
-	public static String getDigestUserPwd(String id) throws Exception {
-		return DigestAuthenticationProvider.generateDigest(id);
+	public static String getDigestUserPwd(String id) {
+		String digest = "";
+		try {
+			digest = DigestAuthenticationProvider.generateDigest(id);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return digest;
 	}
 	
 	public static void main(String[] args) throws IOException, InterruptedException, KeeperException, Exception {
